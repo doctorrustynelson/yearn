73e0c2a43bfa842e382d54d088b424c2004a7e9a
Rusty Nelson <doctor.rusty.nelson@gmail.com>
2014-11-15 14:55:29 -0500

    Added support for npm test and published to npm with version 0.0.4.

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,5 +32,6 @@ module.exports = function( grunt ){
 	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
 	grunt.loadNpmTasks( 'grunt-contrib-nodeunit' );
 	
-	grunt.registerTask( 'default', [ 'jshint', 'nodeunit' ] );
+	grunt.registerTask( 'test', [ 'jshint', 'nodeunit' ] );
+	grunt.registerTask( 'default', [ 'test' ] );
 };
