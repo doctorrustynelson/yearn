6b748b3039a0cdd6b854b6e57a11888b848994ea
Rusty Nelson <doctor.rusty.nelson@gmail.com>
2014-11-15 15:13:11 -0500

    Added .travis.yml for travis CI build

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,7 +31,8 @@ module.exports = function( grunt ){
 	
 	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
 	grunt.loadNpmTasks( 'grunt-contrib-nodeunit' );
+	grunt.loadNpmTasks( 'grunt-travis-lint' );
 	
-	grunt.registerTask( 'test', [ 'jshint', 'nodeunit' ] );
+	grunt.registerTask( 'test', [ 'jshint', 'travis-lint', 'nodeunit' ] );
 	grunt.registerTask( 'default', [ 'test' ] );
 };
