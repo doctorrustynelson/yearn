692db53d91aa135f1c914e6ae6e8dead9702584b
Rusty Nelson <doctor.rusty.nelson@gmail.com>
2015-04-14 08:57:05 -0400

    Removed custom override force test because it messes up coverage.

--- tests/yearn-override-function-tests.js
+++ tests/yearn-override-function-tests.js
@@ -30,12 +30,11 @@
 
 var path = require( 'path' );
 var path_resolved = require.resolve( 'path' );
-var raw_yearn = null;
 var yearn = null;
 
 module.exports.setUp = function( callback ){
 	var original_resolver = module.constructor._resolveFilename;
-	raw_yearn = require( '../lib/yearn' );
+	
 	yearn = require( '../lib/yearn' )({ 
 		orgs: { 
 			'': './node_modules',
@@ -46,6 +45,7 @@ module.exports.setUp = function( callback ){
 			return original_resolver( 'path', parent );
 		}
 	} );
+	
 	callback( );
 };
 
@@ -61,26 +61,6 @@ module.exports.tearDown = function( callback ){
 	
 	callback( );
 };
-
-/*
-module.exports.forceTest = function( test ){
-	
-	test.notStrictEqual( yearn._originalResolver, undefined, 'yearn._originalResolver is undefined when not overriding' );
-
-	var new_yearn = raw_yearn({ 
-		orgs: { 
-			'': './node_modules',
-			'test_modules': path.join( __dirname, 'node_modules' ) 
-		},
-		override: false
-	}, true );
-	
-	test.strictEqual( new_yearn._originalResolver, undefined, 'yearn._originalResolver is undefined when not overriding' );
-	test.notStrictEqual( new_yearn, yearn, 'Cached yearn returned on second require.' );
-	
-	test.done();
-};
-*/
 
 module.exports.nativeRequireTests = {
 		
