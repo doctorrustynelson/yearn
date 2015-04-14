e6f3e5ec495094832127ef9557cadf22da164726
Rusty Nelson <doctor.rusty.nelson@gmail.com>
2015-04-14 07:58:52 -0400

    Refactored custom override tests setup

--- tests/yearn-override-function-tests.js
+++ tests/yearn-override-function-tests.js
@@ -30,12 +30,11 @@
 
 var path = require( 'path' );
 var path_resolved = require.resolve( 'path' );
-var raw_yearn = require( '../lib/yearn' );
 var yearn = null;
 
 module.exports.setUp = function( callback ){
 	var original_resolver = module.constructor._resolveFilename;
-	yearn = raw_yearn({ 
+	yearn = require( '../lib/yearn' )({ 
 		orgs: { 
 			'': './node_modules',
 			'test_modules': path.join( __dirname, 'node_modules' )
@@ -65,7 +64,7 @@ module.exports.forceTest = function( test ){
 	
 	test.notStrictEqual( yearn._originalResolver, undefined, 'yearn._originalResolver is undefined when not overriding' );
 
-	var new_yearn = raw_yearn({ 
+	var new_yearn = require( '../lib/yearn' )({ 
 		orgs: { 
 			'': './node_modules',
 			'test_modules': path.join( __dirname, 'node_modules' ) 
